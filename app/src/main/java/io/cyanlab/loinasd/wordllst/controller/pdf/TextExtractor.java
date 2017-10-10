a26f5cbea6de7a304955737d30059c4f0168c8f9
LoinASD <levkiselev@gmail.com>
2017-10-10 16:04:38 +0700

    refactoring

--- app/src/main/java/io/cyanlab/loinasd/wordllst/controller/pdf/TextExtractor.java
+++ app/src/main/java/io/cyanlab/loinasd/wordllst/controller/pdf/TextExtractor.java
@@ -8,60 +8,74 @@ import java.util.ArrayList;
 public class TextExtractor {
 
     private static int ch;
-    private static char cc;
     private final InputStream io;
-    private ArrayList<String> ar = new ArrayList<>();
     private ArrayList<Node> nodes = new ArrayList<>();
-    private StringBuilder prev = new StringBuilder();
     private CharConverter converter = new CharConverter();
+    private static String lineStr;
+    private static boolean gotDictionary = false;
 
     public TextExtractor(InputStream io) throws IOException {
         this.io = io;
         ch = 0;
-        while (ch != -1) {
-            ch = io.read();
-            cc = (char) ch;
-            if ((char) ch == 'B') {
-                ch = io.read();
-                cc = (char) ch;
-                if ((char) ch == 'T') {
-                    textToken();
-                }
+
+        parse();
+        if (gotDictionary){
+            for (Node node: nodes) {
+                node.convertText(converter);
+                System.out.println(node.getText());
             }
         }
-        for (Node node: nodes) {
-            node.convertText(converter);
-            System.out.println(node.getText());
-        }
 
+
+    }
+
+    private void parse() throws IOException {
+        while (ch != -1) {
+            lineStr = readLine();
+            if (lineStr.equals("BT")) {
+                textToken();
+            } else if (lineStr.equals("begincmap")){
+                //TODO: parse cmap
+            }
+
+        }
+    }
+
+    private void getCord(Node node) throws IOException {
+        ch = io.read();
+        while ((char) ch != '[') {
+            lineStr = readLine();
+            if (lineStr.endsWith("Tm")) {
+                String[] cord = lineStr.split(" ");
+                node.setX(Double.parseDouble(cord[4]));
+                node.setY(Double.parseDouble(cord[5]));
+            }
+            ch = io.read();
+        }
+    }
+
+    private String readLine() throws IOException {
+        StringBuilder l = new StringBuilder();
+        while (ch != -1) {
+            ch = io.read();
+
+            if ((char) ch != '\n') {
+                l.append((char) ch);
+            } else {
+                return l.toString();
+            }
+        }
+        return null;
     }
 
     private void textToken() throws IOException {
         StringBuilder message = new StringBuilder();
-        StringBuffer lineBuf = new StringBuffer();
-        String line;
         Node node = new Node();
-        //node.convertText(converter);
-        ch = io.read();
-
-        while ((char) ch != '[') {
-            cc = (char) ch;
-            if (cc != '\n') {
-                lineBuf.append(cc);
-            } else {
-                line = lineBuf.toString();
-                lineBuf = new StringBuffer();
-                if (line.endsWith("Tm")) {
-                    String[] cord = line.split(" ");
-                    node.setX(Double.parseDouble(cord[4]));
-                    node.setY(Double.parseDouble(cord[5]));
-                }
-            }
-            ch = io.read();
-        }
+        getCord(node);
         ch = io.read();
         while ((char) ch != ']'){
             message.append((char) ch);
+            ch = io.read();
         }
         node.setRawText(message.toString().trim());
         nodes.add(node);
